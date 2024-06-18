77808d5c965c68a6c8d63b39b455c62cc74e9a8e
Gluigi75 <gian.luigi.serra@it.ey.com>
2024-06-18 13:14:06 +0200

    fix log e file js

--- index.php
+++ index.php
@@ -1,3 +1,156 @@
 <?php
+	//require_once("jsSerialApi.js");
 
-echo "Hello World 2023!";
+function sendData(){
+	
+	echo "<button id='connectBtn' onclick='startSerialCommunication()'>Connect</button>";
+    //echo "<button onclick='stopSerialCommunication()'>Disconnect</button>"
+    echo "  <button id='sendBtn' onclick='sendData()' disabled='true'>Send</button>";
+	
+
+	$tipoMsg = "113";
+
+	$tmp = "$tipoMsg~0008";
+/*
+	echo "<form name id='form1' method='post' action='/opt/web/php/accesso/awp/index.php/sessione/inviorichieste' enctype='application/x-www-form-urlencoded'>\n";
+*/
+	echo "<form name='form1' id='form1' method='post' action='index.php' enctype='application/x-www-form-urlencoded'>\n";
+	// echo "<input type='hidden'  id='com' 		name='com' 		value='{$com}'>\n";
+	// echo "<input type='hidden'  id='idEnte'		name='idEnte' 		value='{$_SESSION['AWP']['id_ente_old']}'>\n";
+	// echo "<input type='hidden'  id='idVerifica'	name='idVerifica' 	value='{$_SESSION['AWP']['id_ver']}'>\n";
+	// echo "<input type='hidden'  id='richiesta' 	name='richiesta' 	value={$ricCryptToCard}>\n";
+	// echo "<input type='hidden'  id='risposta' 	name='risposta' 	value='prova'>\n";
+	echo "<input type='hidden'  id='rispostaCom' 	name='rispostaCom' 	value='prova'>\n";
+	echo "<input type='hidden'  id='messaggio' 	name='messaggio'	value='{$tmp}'>\n";
+	// echo "<input type='hidden'  id='codeid' 	name='codeid' 		value='{$codeid}'>\n";   ///per passarlo quando ricarica la pag...... bah
+	// echo "<input type='hidden'  id='tipoMsg'	name='tipoMsg' 		value={$tipoMsg}>\n";
+	//echo "<script type='text/javascript'>\n";
+	//echo "alert(',,,,,,,,,'+document.getElementById('rispostaCom').value);\n";
+	//echo "sendData();\n";
+	//echo "document.getElementById('form1').submit();\n";
+	//echo "</script>\n";
+    //echo "<input type='submit' value='submit'>\n";
+	echo "</form >\n";
+
+	echo "<script type='text/javascript'>\n";
+	require_once("jsSerialApi.js");
+	echo "</script>\n";
+
+}
+
+
+function hexToStr($hex){
+	$string='';
+	for ($i=0; $i < strlen($hex)-1; $i+=2){
+		$string .= chr(hexdec($hex[$i].$hex[$i+1]));
+	}
+	return $string;
+}
+
+
+if($_POST){
+
+
+    foreach  ($_POST  as $key => $value){
+        //echo "$key => $value";
+    
+        $sel["$key"]     = urldecode($value);
+    }
+
+
+	// $codeid		= isset($sel['codeid'])?$sel['codeid']:null;
+	// $com		= isset($sel['com'])?$sel['com']:null;
+	// $tipoMsg	= isset($sel['tipoMsg'])?$sel['tipoMsg']:null;
+	// $risposta	= isset($sel['risposta'])?$sel['risposta']:null;
+	$rispostaCom	= isset($sel['rispostaCom'])?$sel['rispostaCom']:null;
+	//$richiesta	= isset($sel['richiesta'])?$sel['richiesta']:null;
+	$pattern = '/^[a-fA-F0-9]{16,16}$/';
+
+	// $_SESSION['AWP']['id_ente_old']=isset($sel['idEnte'])?$sel['idEnte']:$_SESSION['AWP']['id_ente_old'];
+	// $_SESSION['AWP']['id_ver']=isset($sel['idVerifica'])?$sel['idVerifica']:$_SESSION['AWP']['id_ver'];
+
+	if($rispostaCom){
+		
+		if($rispostaCom == 'prova' || $rispostaCom == null){
+
+			$rc="problemi con activex";
+			$fase = 4;
+			//$rc= getValue('help.txt',$fase);
+		}else{
+			//exit;
+			//$tmp1=explode('#',$richiesta);
+
+
+			$rispostaCom=hexToStr($rispostaCom);
+
+            $itemRis= explode('~',$rispostaCom);
+
+            // $data['CNTTOTIN']	= intval($itemRis[5]);
+            // $data['CNTTOTOT']	= intval($itemRis[6]);
+            // $data['CNTCL']		= intval($itemRis[7]);
+            // $data['CNTOT']		= intval($itemRis[9]);
+            // $data['CNTNP']		= intval($itemRis[10]);
+            // $data['CNTIN']		= intval($itemRis[8]);
+
+            // $message = 'CNTTOTIN:'. $data['CNTTOTIN'] .'<br>
+            // CNTTOTOT:'. $data['CNTTOTOT'].'<br>
+            // CNTCL:'. $data['CNTCL']	.'<br>
+            // CNTOT:'. $data['CNTOT']	.'<br>
+            // CNTNP:'. $data['CNTNP']	.'<br>	
+            // CNTIN:'. $data['CNTIN']	.'<br>';
+            
+            //echo $message;
+
+
+			$response = "########################################<br>
+			Id_messaggio: " . $itemRis[0] . "<br>
+			L_messaggio: " . $itemRis[1] . "<br>
+			DATA_Risposta: " . $itemRis[2] . "<br>
+			CODEID: " . $itemRis[3] . "<br>
+			ESITO: " . $itemRis[4] . "<br>
+			Valore contatore CNTTOTIN: " . $itemRis[5] . "<br>
+			Valore contatore CNTTOTOT: " . $itemRis[6] . "<br>
+			Valore contatore CNTCL: " . $itemRis[7] . "<br>
+			Valore contatore CNTIN: " . $itemRis[8] . "<br>
+			Valore contatore CNTOT: " . $itemRis[9] . "<br>
+			Valore contatore CNTNP: " . $itemRis[10] . "<br>
+			ORA_Risposta: " . $itemRis[11] . "<br>
+			Codice_autenticazione: " . $itemRis[12] . "<br>
+			Esito_comunicazione: " . $itemRis[13] . "<br>
+			########################################<br>";
+
+			echo $response;
+
+			echo "<script type='text/javascript'>\n";
+			echo "window.onload = function(){
+				document.getElementById('connectBtn').style.display = 'none';
+				document.getElementById('sendBtn').style.display = 'none';
+			}";
+			echo "</script>\n";
+
+			//echo $rispostaCom;
+			//die();
+        }
+    }
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+
+</head>
+<body>
+    <!-- <button onclick="startSerialCommunication()">Connect</button>
+    <button onclick="stopSerialCommunication()">Disconnect</button>
+    <button onclick="sendData()">Send</button> -->
+    <?php
+        sendData();
+    ?>
+    <!-- <script type="text/javascript" src='jsSerialApi.js'></script> -->
+</body>
+</html>
---
+++ jsSerialApi.js
@@ -0,0 +1,153 @@
+let port, ports, textEncoder, writer, writableStreamClosed = -1;
+
+function translateConvertedChar2ClearChar(inputString) {
+	var campoOut = "";
+	var byteOut = "";
+	var arr = new Array();
+	var campoWorking = inputString;
+
+	if (inputString == null || inputString == "") {
+		return campoOut;
+	}
+
+	for (i = 0; i < inputString.length; i++) {
+		codice = inputString.charCodeAt(i);
+		byteOut = d2h(codice);
+		byteOut = byteOut.toUpperCase();
+
+		if (byteOut == 1 || byteOut == 2 || byteOut == 3 || byteOut == 4 || byteOut == 5 || byteOut == 6 || byteOut == 7 || byteOut == 8 || byteOut == 9 || byteOut == 'A' || byteOut == 'B' || byteOut == 'C' || byteOut == 'D' || byteOut == 'E' || byteOut == 'F') {
+
+
+			byteOut = '0' + byteOut;
+
+		}
+		if (byteOut == 100) {
+			byteOut = '00';
+		}
+
+		campoOut = campoOut + byteOut;
+	
+		arraylen = arr.push(byteOut);
+	}
+	return campoOut;
+}
+
+function sendData(){
+    let txt = document.getElementById("messaggio").value;
+    console.log("Messaggio: " + txt);
+    writeSerialData(txt)
+}
+
+
+navigator.serial.addEventListener('connect', e => {
+  alert("connected");
+});
+
+ // Richiedi l'accesso alla porta seriale
+async function requestSerialPort() {
+  if ("serial" in navigator) {
+    console.log("Web Serial API is supported.");
+    try {
+        port = await navigator.serial.requestPort();
+        await port.open({ baudRate: 19200 }); // Configura la velocità di baud rate appropriata
+        let settings = {};
+
+        if (localStorage.dtrOn == "true") settings.dataTerminalReady = true;
+        if (localStorage.rtsOn == "true") settings.requestToSend = true;
+        if (Object.keys(settings).length > 0) await port.setSignals(settings);
+        
+        textEncoder = new TextEncoderStream();
+        writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
+        writer = textEncoder.writable.getWriter();
+        
+        return port;
+        
+    } catch (error) {
+        console.error("Errore durante la richiesta della porta seriale:", error);
+    }
+  } else {
+    console.error("Web Serial API not supported.");
+  }
+}
+
+// Leggi dati dalla porta seriale
+async function readSerialData(port) {
+    const reader = port.readable.getReader();
+    let app = '';
+    let ricev = '';
+    while (true) {
+        try {
+            const { value, done } = await reader.read();
+            if (done) break;
+           console.log("");
+            let arr = value;
+            
+            for (x = 0; x < arr.length; x++) {
+                if (arr[x] == 0) {
+                    arr[x] = 256;
+                }
+                //conversione dei singoli byte da charcode a carattere
+                ricev = String.fromCharCode(arr[x]);
+                app = app + ricev;
+            }
+            
+            //console.log("Ricevuto:" + app);
+            
+            
+            let translate = translateConvertedChar2ClearChar(app);
+            //console.log("translate: " + translate)
+            document.getElementById("rispostaCom").value=translate;
+            document.getElementById('form1').submit();
+            //------------------------------------------
+            
+        } catch (error) {
+          console.error("Errore durante la lettura dati seriali:", error);
+          break;
+        }
+    }
+
+    reader.releaseLock();
+}
+
+// Scrivi dati sulla porta seriale
+async function writeSerialData(data) {
+  try {
+    await writer.write(data + "\n");
+    console.log("Dati seriali inviati con successo.");
+  } catch (error) {
+    console.error("Errore durante l'invio dati seriali:", error);
+  } 
+}
+
+function stopSerialCommunication(){
+    writer.releaseLock();
+}
+
+// Esempio di utilizzo
+async function startSerialCommunication() {
+  port = await requestSerialPort();
+
+  if (port) {
+    console.log("Porta selezionata")
+    await readSerialData(port); 
+    conaole.log("Lettura su porta selezionata!");
+    await writeSerialData(port, "Dati da inviare sulla porta seriale");
+    conaole.log("Scrittura su porta selezionata!");
+  }
+  else {
+    console.log("Porta non trovata!")
+  }
+}
+
+function h2d(h) { return parseInt(h, 16); }
+
+function d2h(d) { return d.toString(16); }
+
+const connBtn = document.getElementById('connectBtn');
+connBtn.addEventListener('click', () => {
+  connBtn.disabled = true;
+  const sendBtn = document.getElementById('sendBtn');
+  sendBtn.disabled = false;
+});
+
+
