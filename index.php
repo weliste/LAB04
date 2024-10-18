1a0c434b41cb44561fc34471c20271a601dc9492
emericjego-alliance <emeric.jego@alliancemarine.com>
2024-10-18 15:29:45 +0200

    test azure 4

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-
-echo "Hello TEST AZURE 3!";
+<html>
+    <head>
+        <title>Ma page d'accueil </title>
+    </head>
+    <body>
+        <h1>Bienvenue sur le site de toto </h1>
+        <p> Toto fait de l'anglais :</p>
+        <?php
+        echo '<p>Hello ! What is the day today ? It is '.date("l").' !</p>';
+        ?>
+    </body>
+</html>
